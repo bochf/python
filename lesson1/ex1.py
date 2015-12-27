d424f739080310932641f4cad850b2c6b781b737
Josh Chen <bochfchen.code@gmail.com>
2015-12-26 23:11:21 -0500

    utf8 support

--- lesson1/ex1.py
+++ lesson1/ex1.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 print "Hello World!"
 print "Hello Again"
 print "I like type this."
@@ -5,3 +7,4 @@ print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+print u'如何显示我自己国家的文字'
